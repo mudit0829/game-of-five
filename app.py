dc88f3d202a2409abc630c4c3ee051a61d477b13
mudit0829 <mekasutechnology@gmail.com>
2026-05-03 16:37:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -799,7 +799,7 @@ user_game_history = {}
 # ---------------------------------------------------
 
 ROUND_SECONDS = 300  # 5 minutes
-ROULETTE_ROUND_SECONDS = 3600  # 60 minutes for roulette game
+ROULETTE_ROUND_SECONDS = 120  # 60 minutes for roulette game
 
 
 def _floor_epoch(ts: int, period: int) -> int:
